f7a115332cb65bb73ad637ade632c1e2b101d776
ColRealPro <64316078+ColRealPro@users.noreply.github.com>
2024-06-10 15:40:40 -0500

    Add join and leave messages

--- src/main/java/me/colrealpro/mcdiscord/server/systems/PlayerJoinMessages.java
+++ src/main/java/me/colrealpro/mcdiscord/server/systems/PlayerJoinMessages.java
@@ -4,16 +4,42 @@ import me.colrealpro.mcdiscord.MCDiscord;
 import me.colrealpro.mcdiscord.events.EventHandler;
 import me.colrealpro.mcdiscord.events.game.PlayerJoinEvent;
 import me.colrealpro.mcdiscord.events.game.PlayerLeaveEvent;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 
 public class PlayerJoinMessages {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         MCDiscord.LOGGER.info("{} joined the server (event)", event.getPlayer().getName().getString());
+
+        sendJoinEmbed(event.getPlayer().getName().getString(), true);
     }
 
     @EventHandler
     public void onPlayerLeave(PlayerLeaveEvent event) {
         MCDiscord.LOGGER.info("{} left the server (event)", event.getPlayer().getName().getString());
+
+        sendJoinEmbed(event.getPlayer().getName().getString(), false);
+    }
+
+    private void sendJoinEmbed(String playerName, boolean wasJoin) {
+        String title = wasJoin ? "Player Joined!" : "Player Left!";
+        String playerJoinMessage = wasJoin ? "%s has joined the server" : "%s has left the server";
+        String onlinePlayers = " (%s players online)";
+
+        int playerCount = MCDiscord.getServer().getCurrentPlayerCount();
+        int color = wasJoin ? 0x73ff40 : 0xff4c38;
+        String description = String.format(playerJoinMessage + onlinePlayers, playerName, playerCount);
+
+        MessageEmbed embed = new EmbedBuilder()
+            .setTitle(title)
+            .setColor(color)
+            .setDescription(description)
+            .build();
+
+        MCDiscord.discordBot.getBot().getGuildById(MCDiscord.getGuildID()).getChannelById(TextChannel.class, MCDiscord.getDefaultChannelID())
+            .sendMessageEmbeds(embed).queue();
     }
 }
