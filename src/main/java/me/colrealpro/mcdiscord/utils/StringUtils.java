5e2e99084cbdaaab381958b4b90ec39d7f3035ef
ColRealPro <64316078+ColRealPro@users.noreply.github.com>
2024-06-10 22:46:59 -0500

    Add string utils

---
+++ src/main/java/me/colrealpro/mcdiscord/utils/StringUtils.java
@@ -0,0 +1,12 @@
+package me.colrealpro.mcdiscord.utils;
+
+public class StringUtils {
+
+    public static String capitalize(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+
+}
