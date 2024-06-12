fe205e03b9685d367fdab57f190ca47e849698b8
ColRealPro <64316078+ColRealPro@users.noreply.github.com>
2024-06-11 22:06:12 -0500

    Fix commands to properly handle arguments

--- src/main/java/me/colrealpro/mcdiscord/server/Command.java
+++ src/main/java/me/colrealpro/mcdiscord/server/Command.java
@@ -1,17 +1,25 @@
 package me.colrealpro.mcdiscord.server;
 
+import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import me.colrealpro.mcdiscord.MCDiscord;
+import me.colrealpro.mcdiscord.server.suggestions.DiscordChannelSuggestionProvider;
 import net.minecraft.server.command.ServerCommandSource;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.server.command.CommandManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Command {
-    final String commandName;
-    final Argument[] arguments;
+    private final String commandName;
+    private final Argument[] arguments;
+    private int amountOfArguments = 0;
 
     public Command(String commandName) {
         this.commandName = commandName.toLowerCase();
@@ -25,6 +33,7 @@ public class Command {
         for (int i = 0; i < arguments.length; i++) {
             if (arguments[i] == null) {
                 arguments[i] = argument;
+                amountOfArguments++;
                 break;
             }
 
@@ -41,20 +50,62 @@ public class Command {
     public void build() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             LiteralArgumentBuilder<ServerCommandSource> commandLiteral = CommandManager.literal(commandName);
+            List<RequiredArgumentBuilder<ServerCommandSource, ?>> argumentList = new ArrayList<>();
 
+            int argumentCounter = 0;
             for (Argument argument : arguments) {
                 if (argument == null) {
                     break;
                 }
 
-                switch (argument.getType()) {
-                    case INTEGER -> commandLiteral.then(CommandManager.argument(argument.getName(), IntegerArgumentType.integer()));
-                    case STRING -> commandLiteral.then(CommandManager.argument(argument.getName(), StringArgumentType.string()));
-                    case BOOLEAN -> commandLiteral.then(CommandManager.argument(argument.getName(), BoolArgumentType.bool()));
+                argumentCounter++;
+
+                if (MCDiscord.debugEnabled()) {
+                    MCDiscord.LOGGER.info("Registering argument: {}", argument.getName());
+                }
+
+                ArgumentType<?> argumentType = switch (argument.getType()) {
+                    case INTEGER -> IntegerArgumentType.integer();
+                    case STRING, DISCORD_CHANNEL -> StringArgumentType.string();
+                    case BOOLEAN -> BoolArgumentType.bool();
+                };
+
+                RequiredArgumentBuilder<ServerCommandSource, ?> newArgument = CommandManager.argument(argument.getName(), argumentType);
+
+                if (argument.getType() == CommandType.DISCORD_CHANNEL) {
+                    newArgument.suggests(new DiscordChannelSuggestionProvider());
+                }
+
+                argumentList.add(newArgument);
+
+                if (argumentCounter == amountOfArguments) {
+                    RequiredArgumentBuilder<ServerCommandSource, ?> lastArgument = null;
+                    List<RequiredArgumentBuilder<ServerCommandSource, ?>> reversedArgumentList = new ArrayList<>();
+
+                    for (int i = argumentList.size() - 1; i >= 0; i--) {
+                        reversedArgumentList.add(argumentList.get(i));
+                    }
+
+                    for (RequiredArgumentBuilder<ServerCommandSource, ?> currentArgument : reversedArgumentList) {
+                        if (lastArgument == null) {
+                            if (MCDiscord.debugEnabled()) MCDiscord.LOGGER.info("Starting stack with executes on: {}", currentArgument.getName());
+                            lastArgument = currentArgument.executes(this::onExecute);
+                        } else {
+                            if (MCDiscord.debugEnabled()) MCDiscord.LOGGER.info("Stacking argument: {}", currentArgument.getName());
+                            lastArgument = currentArgument.then(lastArgument);
+                        }
+                    }
+
+                    if (MCDiscord.debugEnabled()) MCDiscord.LOGGER.info("Registering command: {}, with argument, {}", commandName, lastArgument.getName());
+                    commandLiteral.then(lastArgument);
                 }
             }
 
-            dispatcher.register(commandLiteral.executes(this::onExecute));
+            if (amountOfArguments == 0) {
+                commandLiteral.executes(this::onExecute);
+            }
+
+            dispatcher.register(commandLiteral);
         });
     }
 
