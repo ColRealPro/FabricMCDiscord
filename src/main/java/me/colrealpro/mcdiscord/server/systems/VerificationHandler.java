9461a545b593f0e90d7ead0c5b62e67be4690616
ColRealPro <64316078+ColRealPro@users.noreply.github.com>
2024-06-12 21:24:11 -0500

    Add VerificationHandler

---
+++ src/main/java/me/colrealpro/mcdiscord/server/systems/VerificationHandler.java
@@ -0,0 +1,128 @@
+package me.colrealpro.mcdiscord.server.systems;
+
+import dev.dejvokep.boostedyaml.YamlDocument;
+import me.colrealpro.mcdiscord.MCDiscord;
+import me.colrealpro.mcdiscord.config.ConfigHandler;
+import me.colrealpro.mcdiscord.events.EventHandler;
+import me.colrealpro.mcdiscord.events.discord.MessageEvent;
+import me.colrealpro.mcdiscord.events.game.PlayerAttemptLoginEvent;
+import me.colrealpro.mcdiscord.utils.StringUtils;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+import java.util.UUID;
+
+public class VerificationHandler {
+    private static final ConfigHandler playerData = MCDiscord.loadConfig("playerData.yml");
+
+    @EventHandler
+    @SuppressWarnings("deprecation")
+    public void onPlayerAttemptLogin(PlayerAttemptLoginEvent event) {
+        if (isVerificationRequired()) {
+            YamlDocument config = getDirectConfig();
+
+            if (config == null || playerData == null) {
+                event.setCancelled(true);
+                event.setKickReason(Text.literal("An error occurred while trying to verify your account (ID: NO_DATA_FOUND)").formatted(Formatting.RED));
+                return;
+            }
+
+            String discordID = config.getString("Users." + event.getUUID() + ".DiscordID");
+            if (discordID != null) {
+                return;
+            }
+
+            String verificationCode = getVerificationCode(event.getUUID(), event.getName());
+
+            MutableText kickReason = Text.literal("Your account is not linked with discord!").formatted(Formatting.BLUE)
+                .append(Text.literal("\nLink your account by sending ").formatted(Formatting.WHITE))
+                .append(Text.literal(MCDiscord.discordBot.getBot().getSelfUser().getAsTag()).formatted(Formatting.YELLOW))
+                .append(Text.literal(" the following characters").formatted(Formatting.WHITE))
+                .append(Text.literal("\n\n" + verificationCode).formatted(Formatting.YELLOW)
+                .append(Text.literal("\n\nIf it fails to verify you please reconnect for a new verification code").formatted(Formatting.WHITE)));
+
+            event.setCancelled(true);
+            event.setKickReason(kickReason);
+        }
+    }
+
+    @EventHandler
+    public void onPrivateMessage(MessageEvent event) {
+        if (!event.isPrivateMessage()) {
+            return;
+        }
+
+        String message = event.getMessage();
+
+        // No need to check for a verification code if the message is not the correct length
+        if (message.length() != MCDiscord.config.getDirectConfig().getInt("VerificationCodeLength")) {
+            event.getRawEvent().getMessage().reply("Invalid verification code length").queue();
+            return;
+        }
+
+        YamlDocument config = getDirectConfig();
+
+        if (config == null || playerData == null) {
+            event.getRawEvent().getMessage().reply("An error occurred while trying to verify your account (ID: NO_DATA_FOUND)").queue();
+            return;
+        }
+
+        if (!config.contains("VerificationCodes." + message)) {
+            event.getRawEvent().getMessage().reply("Invalid verification code").queue();
+            return;
+        }
+
+        UUID playerUUID = UUID.fromString(config.getString("VerificationCodes." + message + ".PlayerUUID"));
+        String playerName = config.getString("Users." + playerUUID + ".CachedUsername");
+
+        config.set("Users." + playerUUID + ".DiscordID", event.getAuthor().getId());
+        config.remove("Users." + playerUUID + ".CachedUsername");
+        config.remove("Users." + playerUUID + ".ActiveVerificationCode");
+        config.remove("VerificationCodes." + message);
+
+        playerData.save();
+
+        event.getRawEvent().getMessage().reply("You have been successfully linked to the Minecraft Account: **" + playerName + "**").queue();
+    }
+
+    private static YamlDocument getDirectConfig() {
+        if (playerData == null) {
+            MCDiscord.LOGGER.error("Failed to load playerData.yml (1) - THIS IS LIKELY YOUR FAULT ILL BE HONEST, BUT REPORT IT ANYWAY ON GITHUB");
+            return null;
+        }
+
+        YamlDocument config = playerData.getDirectConfig();
+
+        if (config == null) {
+            MCDiscord.LOGGER.error("Failed to load playerData.yml (2) - THIS IS LIKELY YOUR FAULT ILL BE HONEST, BUT REPORT IT ANYWAY ON GITHUB");
+            return null;
+        }
+
+        return config;
+    }
+
+    public static String getVerificationCode(UUID playerUUID, String username) {
+        YamlDocument config = getDirectConfig();
+
+        if (config == null || playerData == null) {
+            return "An error occurred while trying to get your verification code";
+        }
+
+        String code = config.getString("Users." + playerUUID.toString() + ".ActiveVerificationCode");
+        if (code == null) { // code has not been generated yet
+            code = StringUtils.generateSalt(MCDiscord.config.getDirectConfig().getInt("VerificationCodeLength"));
+            config.set("Users." + playerUUID + ".CachedUsername", username);
+            config.set("Users." + playerUUID + ".ActiveVerificationCode", code);
+            config.set("VerificationCodes." + code + ".PlayerUUID", playerUUID.toString());
+            config.set("VerificationCodes." + code + ".TimeGenerated", System.currentTimeMillis());
+            playerData.save();
+        }
+
+        return code;
+    }
+
+    public boolean isVerificationRequired() {
+        return MCDiscord.config.getDirectConfig().getBoolean("VerificationRequired");
+    }
+}
