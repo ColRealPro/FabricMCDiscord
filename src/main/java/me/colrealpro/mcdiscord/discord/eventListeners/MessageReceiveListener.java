52dfd5a4b2903f3b6cf359cab8af4a3d776534f4
ColRealPro <64316078+ColRealPro@users.noreply.github.com>
2024-06-12 19:22:45 -0500

    Remove unused imports

--- src/main/java/me/colrealpro/mcdiscord/discord/eventListeners/MessageReceiveListener.java
+++ src/main/java/me/colrealpro/mcdiscord/discord/eventListeners/MessageReceiveListener.java
@@ -1,14 +1,10 @@
 package me.colrealpro.mcdiscord.discord.eventListeners;
 
-import me.colrealpro.mcdiscord.MCDiscord;
 import me.colrealpro.mcdiscord.events.EventBus;
 import me.colrealpro.mcdiscord.events.discord.MessageEvent;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 
 public class MessageReceiveListener extends ListenerAdapter {
     @Override
