5cfabddcb29471a1b5f8d4311eef4b15bd950d3a
ColRealPro <64316078+ColRealPro@users.noreply.github.com>
2024-06-13 01:26:11 -0500

    Add a check for being already verified

--- src/main/java/me/colrealpro/mcdiscord/server/commands/VerifyCommand.java
+++ src/main/java/me/colrealpro/mcdiscord/server/commands/VerifyCommand.java
@@ -5,6 +5,7 @@ import me.colrealpro.mcdiscord.MCDiscord;
 import me.colrealpro.mcdiscord.server.Command;
 import me.colrealpro.mcdiscord.server.systems.VerificationHandler;
 import me.colrealpro.mcdiscord.utils.NotificationBuilder;
+import net.dv8tion.jda.api.entities.User;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.MutableText;
@@ -12,8 +13,7 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
 public class VerifyCommand extends Command {
-
-    public VerifyCommand() {
+        public VerifyCommand() {
         super("verify");
     }
 
@@ -27,6 +27,33 @@ public class VerifyCommand extends Command {
             return 0;
         }
 
+        if (VerificationHandler.isPlayerVerified(player.getUuid())) {
+            context.getSource().sendFeedback(() -> NotificationBuilder.getFormatted("Fetching verification data..."), false);
+
+            String discordID = VerificationHandler.getDiscordAccount(player.getUuid());
+
+            if (discordID == null) return 0; // THIS SHOULD NEVER HAPPEN
+
+            User cachedUser = VerificationHandler.getUserFromCache(player.getUuid());
+
+            if (cachedUser == null) {
+                // attempt to get it from the jda cache, however this doesn't always work -_-
+                cachedUser = MCDiscord.discordBot.getBot().getUserById(discordID);
+            }
+
+            if (cachedUser == null) {
+                MCDiscord.discordBot.getBot().retrieveUserById(discordID).queue(user -> {
+                    VerificationHandler.addUserToCache(player.getUuid(), user);
+                    alreadyVerifiedMessage(context, user);
+                });
+            } else {
+                VerificationHandler.addUserToCache(player.getUuid(), cachedUser); // yes, this will overwrite the user with the same user, but it doesn't matter
+                alreadyVerifiedMessage(context, cachedUser);
+            }
+
+            return 0;
+        }
+
         String code = VerificationHandler.getVerificationCode(player.getUuid(), player.getName().getString());
         MutableText message = NotificationBuilder.getFormatted("Please DM the following code to ")
             .append(Text.literal(MCDiscord.discordBot.getBot().getSelfUser().getAsTag()).formatted(Formatting.YELLOW))
@@ -36,5 +63,24 @@ public class VerifyCommand extends Command {
         context.getSource().sendFeedback(() -> message, false);
 
         return 1;
+    }
+
+    public static void alreadyVerifiedMessage(CommandContext<ServerCommandSource> context, User user) {
+        String accountString;
+
+        if (user == null) {
+            accountString = "Unknown User";
+        } else {
+            accountString = String.format("%s (%s)", user.getEffectiveName(), user.getName());
+
+            if (user.getEffectiveName().equals(user.getName())) {
+                accountString = user.getName();
+            }
+        }
+
+        MutableText message = NotificationBuilder.getFormatted("You are already verified to ")
+            .append(Text.literal(accountString).formatted(Formatting.YELLOW));
+
+        context.getSource().sendFeedback(() -> message, false);
     }
 }
